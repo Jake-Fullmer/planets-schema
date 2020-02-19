205422baac5bf8d7ecd0b4b1b1cd36746b04a80c
Jake-Fullmer <jnfu@outlook.com>
2020-02-19 14:56:53 -0700

    create

--- server/controllers/GalaxyController.js
+++ server/controllers/GalaxyController.js
@@ -8,6 +8,7 @@ export default class GalaxyController {
       .Router()
       //NOTE  each route gets registered as a .get, .post, .put, or .delete, the first parameter of each method is a string to be concatinated onto the base url registered with the route in main. The second parameter is the method that will be run when this route is hit.
       .get("", this.getAll)
+      .get("/:id", this.findById)
       .post("", this.create);
   }
 
@@ -15,6 +16,14 @@ export default class GalaxyController {
     try {
       let data = await galaxyService.getAll();
       return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async findById(req, res, next) {
+    try {
+      let data = await galaxyService.findById(req.params.id);
+      res.send(data);
     } catch (error) {
       next(error);
     }
--- server/services/GalaxyService.js
+++ server/services/GalaxyService.js
@@ -8,6 +8,9 @@ class GalaxyService {
   async getAll() {
     return await _repository.find({});
   }
+  async findById(id) {
+    return await _repository.findById(id);
+  }
   async create(rawData) {
     return await _repository.create(rawData);
   }
